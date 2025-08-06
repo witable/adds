156e7189ec58839df805dfdcad1f9a58fad21c1e
witable <isaac@whateverany.com>
2025-08-06 12:09:55 +0930

    final

--- Player.h
+++ Player.h
@@ -2,16 +2,22 @@
 #define PLAYER_H
 
 #include <iostream>
+#include "Move.h"
+#include "MoveMaker.h"
+
 using namespace std;
 
 class Player
 {
     protected:
         string name;
+        MoveMaker moveMaker;
+        Move move;
+        
     public:
         Player();
         string getName();
-        virtual char makeMove() = 0;
+        virtual Move* makeMove() = 0;
         Player(string name);
 };
 
