db205a3855d63a9b2bf19204e756cac5f9b206f1
witable <isaac@whateverany.com>
2025-08-31 19:34:58 +0930

    new code

--- Finder.cpp
+++ Finder.cpp
@@ -5,16 +5,20 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
-
+        size_t start = 0;
+        
         for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i));
+            size_t found = s1.find(s2.substr(0, i), start);
             if (found != string::npos) {
+                start = found;
                 result.push_back(found);
             } else {
                 result.push_back(-1);
+                start++;
             }
         }
         return result;
     }
 
    
+    
