703cac878915752c7655a4716e7c4d3f74427a73
witable <isaac@whateverany.com>
2025-08-31 19:35:23 +0930

    index

--- Finder.cpp
+++ Finder.cpp
@@ -5,16 +5,16 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
-        size_t start = 0;
+        size_t index = 0;
         
         for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i), start);
+            size_t found = s1.find(s2.substr(0, i), index);
             if (found != string::npos) {
-                start = found;
+                index = found;
                 result.push_back(found);
             } else {
                 result.push_back(-1);
-                start++;
+                index++;
             }
         }
         return result;
