1f05aded9ac46b470c763d5e0b989ea6ea7646d1
witable <isaac@whateverany.com>
2025-08-31 20:02:25 +0930

    g

--- Finder.cpp
+++ Finder.cpp
@@ -5,15 +5,9 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
-        result.reserve(s2.size());
         size_t index = 0;
         
         for(size_t i = 1; i <= s2.size(); i++) {
-            if (i > s1.size()) {
-            result.push_back(-1);
-            result.insert(result.end(), s2.size() - i, -1);
-            break;
-        }
             size_t found = s1.find(s2.c_str(), index, i);
             if (found != string::npos) {
                 index = found;
