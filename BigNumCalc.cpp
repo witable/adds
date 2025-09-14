5c5066ddf029b843ea7fe9a6717b11728fc4921f
witable <isaac@whateverany.com>
2025-09-14 23:24:14 +0930

    d

---
+++ BigNumCalc.cpp
@@ -0,0 +1,28 @@
+#include "BigNumCalc.h"
+
+BigNumCalc::BigNumCalc()
+{
+
+}
+
+BigNumCalc::~BigNumCalc()
+{
+    
+}
+
+std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2)
+{
+
+}
+std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2)
+{
+
+}
+std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2)
+{
+
+}
+std::list<int> BigNumCalc::buildBigNum(std::list<int> num1, std::list<int> num2)
+{
+
+}
